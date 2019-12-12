2d9cd617d2b4cc4e4629a3f42de7c89f988c06d6
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 17:45:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function Driver(name,age,experience){
   this.age=age
   this.experience=experience
 }
-function Driver(name,age,experience){
-  this.name=name
-  this.age=age
-  this.experience=experience
+function PickupLocation(address,city){
+  this.address=address
+  this.city=city
+
 }
